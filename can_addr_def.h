5bb28ab08258912c4e22f9049731b2667a76d908
Panda Wu <wu.messi.wu@gmail.com>
2026-05-26 20:00:07 +0800

    feat: add GPS CAN configuration

--- can_addr_def.h
+++ can_addr_def.h
@@ -23,12 +23,70 @@ typedef enum {
   CA_BMS_DATA2 = 0x41,
   CA_LVBMS_DATA1 = 0x45,
   CA_LVBMS_DATA2 = 0x46,
-  
+
   CA_DASHBOARD_BTN = 0x50,
   CA_DAQ_EN = 0x70,
   CA_DAQ_DATA = 0x71,
   CA_RGB_EN = 0x90,
+  CA_IMU_Err = 0x01,
+  CA_IMU_SampleTime = 0x05,
+  CA_IMU_GroupCnt = 0x06,
+  CA_IMU_Status = 0x11,
+  CA_IMU_Quat = 0x21,
+  CA_IMU_dv = 0x31,
+  CA_IMU_RateTurn = 0x32,
+  CA_IMU_dq = 0x33,
+  CA_IMU_ACC = 0x34,
+  CA_IMU_FreeAcc = 0x35,
 } can_addr_def_e;
+
+/* CAN data index for IMU info */
+#define CA_IMU_Err_IDX 0
+#define CA_IMU_SampleTime_IDX 0
+#define CA_IMU_GroupCnt_IDX 0
+#define CA_IMU_Status_IDX 0
+#define CA_IMU_Quat_0_LOW_IDX 0
+#define CA_IMU_Quat_0_HIGH_IDX 1
+#define CA_IMU_Quat_1_LOW_IDX 2
+#define CA_IMU_Quat_1_HIGH_IDX 3
+#define CA_IMU_Quat_2_LOW_IDX 4
+#define CA_IMU_Quat_2_HIGH_IDX 5
+#define CA_IMU_Quat_3_LOW_IDX 6
+#define CA_IMU_Quat_3_HIGH_IDX 7
+#define CA_IMU_dv_x_LOW_IDX 0
+#define CA_IMU_dv_x_HIGH_IDX 1
+#define CA_IMU_dv_y_LOW_IDX 2
+#define CA_IMU_dv_y_HIGH_IDX 3
+#define CA_IMU_dv_z_LOW_IDX 4
+#define CA_IMU_dv_z_HIGH_IDX 5
+#define CA_IMU_dv_exp_LOW_IDX 6
+#define CA_IMU_dv_exp_HIGH_IDX 7
+#define CA_IMU_RateTurn_x_LOW_IDX 0
+#define CA_IMU_RateTurn_x_HIGH_IDX 1
+#define CA_IMU_RateTurn_y_LOW_IDX 2
+#define CA_IMU_RateTurn_y_HIGH_IDX 3
+#define CA_IMU_RateTurn_z_LOW_IDX 4
+#define CA_IMU_RateTurn_z_HIGH_IDX 5
+#define CA_IMU_dq_0_LOW_IDX 0
+#define CA_IMU_dq_0_HIGH_IDX 1
+#define CA_IMU_dq_1_LOW_IDX 2
+#define CA_IMU_dq_1_HIGH_IDX 3
+#define CA_IMU_dq_2_LOW_IDX 4
+#define CA_IMU_dq_2_HIGH_IDX 5
+#define CA_IMU_dq_3_LOW_IDX 6
+#define CA_IMU_dq_3_HIGH_IDX 7
+#define CA_IMU_ACC_x_LOW_IDX 0
+#define CA_IMU_ACC_x_HIGH_IDX 1
+#define CA_IMU_ACC_y_LOW_IDX 2
+#define CA_IMU_ACC_y_HIGH_IDX 3
+#define CA_IMU_ACC_z_LOW_IDX 4
+#define CA_IMU_ACC_z_HIGH_IDX 5
+#define CA_IMU_FreeACC_x_LOW_IDX 0
+#define CA_IMU_FreeACC_x_HIGH_IDX 1
+#define CA_IMU_FreeACC_y_LOW_IDX 2
+#define CA_IMU_FreeACC_y_HIGH_IDX 3
+#define CA_IMU_FreeACC_z_LOW_IDX 4
+#define CA_IMU_FreeACC_z_HIGH_IDX 5
 
 /* CAN data index for Fault Signal */
 #define CA_BSPD_FAULT_IDX 0
@@ -40,7 +98,6 @@ typedef enum {
 #define CA_BSER_FAULT 4
 #define CA_BSEL_FAULT 3
 
-
 /* CAN data index for Dashboard Button */
 #define CA_U1_TRIG 0
 #define CA_U2_TRIG 1
@@ -50,7 +107,6 @@ typedef enum {
 #define CA_U1_LED 0
 #define CA_U2_LED 1
 #define CA_U3_LED 2
-
 
 /* CAN data index for DAQ Enable */
 #define CA_DAQ_EN_IDX 7
